13c0a5fc547f296b43e32b8a38a0eac0c2ac7270
williamsnow <will.distlerneves@gmail.com>
2022-05-15 23:40:18 -0300

    ok

---
+++ main.js
@@ -0,0 +1,19 @@
+// String (textos)
+// Number (number)
+// boolean (true | false)
+
+function onScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
