c1274a965fbcc76e3069e79b9e18a6b34d68ac21
williamsnow <will.distlerneves@gmail.com>
2022-06-14 23:49:47 -0300

    NLW Return

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-// String (textos)
-// Number (number)
-// boolean (true | false)
+window.addEventListener('scroll', onScroll)
 
+onScroll()
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
 }
 
@@ -17,3 +29,18 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 1400
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card,
+  #about,
+  #about header,
+  #about .content`)
